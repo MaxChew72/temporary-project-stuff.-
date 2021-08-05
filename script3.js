3993408ee950de2431a2dc2e1ae46c3ed56dd1a6
MaxChew72 <87897127+MaxChew72@users.noreply.github.com>
2021-08-05 22:36:04 +0800

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,9 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 3.129753736417666 , lng: 101.59581621196189},
+        zoom: 13,
+        mapId: 'dd123f1509656232'
+      });       
+}
+// 3.0437318334217314, 101.64207130064379 <-- Petaling, zoom=18
+// 3.129753736417666, 101.59581621196189 <-- Petaling Jaya, zoom=13

7485fbfe440ebf5373575835beaf5d18d2f43243
MaxChew72 <87897127+MaxChew72@users.noreply.github.com>
2021-08-05 22:39:03 +0800

    Update script3.js

--- script3.js
+++ script3.js
@@ -5,6 +5,6 @@ function initMap(){
         mapId: 'dd123f1509656232'
       });       
 }
-var data = <?php echo json_encode("42", JSON_HEX_TAG); ?>; // Don't forget the extra semicolon!
+var data = <?php echo json_encode("$_GET["IP"]", JSON_HEX_TAG); ?>; // Don't forget the extra semicolon!
 // 3.0437318334217314, 101.64207130064379 <-- Petaling, zoom=18
 // 3.129753736417666, 101.59581621196189 <-- Petaling Jaya, zoom=13
